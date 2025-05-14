796ee7192351387aebe8b32dc651e6d994a78a8b
SuperNours7 <150189089+SuperNours7@users.noreply.github.com>
2025-05-14 03:09:11 -0400

    Delete Gwadien directory

--- Gwadien/events/music/playerStar.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    name: "playerStart",
-    async run(client, queue, track) {
-
-       if(client.blindtests.get(queue.metadata.channel.guildId)?.playlist === track.playlist?.url) return;
-      await queue.metadata.channel.send(`The music \`${track.title}\` during \`${track.duration}\` requested by \`${track.requestedBy.username}\`is playing :)`)
-    }
-};
